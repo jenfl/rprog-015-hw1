1af3088146c033901abc9efeefc6e440c0f59a41
Jen Becker <jenbecker@gmail.com>
2015-06-08 11:36:10 -0400

    First commit

---
+++ pollutantmean.R
@@ -0,0 +1,32 @@
+pollutantmean <- function (directory, pollutant, id=1:332) {
+    
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files.
+    if (!dir.exists(directory)) {
+        print ("ERROR in finding directory")
+        return (FALSE)
+    }
+    
+    ## 'pollutant' is a character vector of length 1 indicating
+    ## the name of the pollutant for which we will calculate the
+    ## mean; either "sulfate" or "nitrate".
+    if (!pollutant %in% c("sulfate", "nitrate")) {
+        print ("ERROR in pollutant, must be sulfate or nitrate")
+        return (FALSE)
+    }
+    
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used.
+    for (i in id) {
+        filename <- paste(formatC(i, width=3, format="d", flag="0"), ".csv", sep="")
+        testfile <- file.path(directory,filename)
+        if (!file.exists(testfile)) {
+            print (c("ERROR in finding file ", testfile))
+            return (FALSE)
+        }
+    }
+    
+    ## Return the mean of the pollutant across all monitors
+    ## listed in the 'id' vector (ignoring NA values).
+    
+}
